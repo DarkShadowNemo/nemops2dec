a93ab0040f24fcb2c7763304bc3bf20f5890e13c
DarkShadowNemo <119450522+DarkShadowNemo@users.noreply.github.com>
2025-01-24 04:06:06 -0800

    TODO

---
+++ nemops2dec/Game/nu_actors/Players/Nemo/nu_decodeTexture1_nemo.py
@@ -0,0 +1,46 @@
+from struct import unpack, pack
+from nu_embedded_textures_nemo import *
+from PIL import Image
+
+def Nemo_Decode_Texture_1_64x64_():
+    if r1 == 232:
+        if g1 == 205:
+            if b1 == 205:
+                if a1 == 127:
+                    if r2 == 0:
+                        if g2 == 0:
+                            if b2 == 0:
+                                if a2 == 127:
+                                    if r3 == 255:
+                                        if g3 == 255:
+                                            if b3 == 255:
+                                                if a3 == 127:
+                                                    if r4 == 237:
+                                                        if g4 == 3:
+                                                            if b4 == 3:
+                                                                if a4 == 127:
+                                                                    if r5 == 185:
+                                                                        if g5 == 100:
+                                                                            if b5 == 100:
+                                                                                if a5 == 127:
+                                                                                    if r6 == 136:
+                                                                                        if g6 == 1:
+                                                                                            if b6 == 1:
+                                                                                                if a6 == 127:
+                                                                                                    if r7 == 220:
+                                                                                                        if g7 == 160:
+                                                                                                            if b7 == 160:
+                                                                                                                if a7 == 127:
+                                                                                                                    if r8 == 188:
+                                                                                                                        if g8 == 43:
+                                                                                                                            if b8 == 43:
+                                                                                                                                if a8 == 127:
+                                                                                                                                    if r9 == 217:
+                                                                                                                                        if g9 == 86:
+                                                                                                                                            if b9 == 86:
+                                                                                                                                                if a9 == 127:
+                                                                                                                                                    if r10 == 36:
+                                                                                                                                                        if g10 == 0:
+                                                                                                                                                            if b10 == 0:
+                                                                                                                                                                if a10 == 127:
+                                                                                                                                                                    pass
